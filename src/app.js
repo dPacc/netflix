7e9552afff0af842e5852fe82b4d4ac49279d654
dPacc <themdk01@gmail.com>
2020-09-29 22:16:55 +0400

    added footer component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
+    <>
       <JumbotronContainer />
-      
+      <FooterContainer />
+    </>
   );
 }
