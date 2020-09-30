47d397cbf5d383404e3aeba07fecb44ae9827614
dPacc <themdk01@gmail.com>
2020-09-30 23:38:02 +0400

    added opt form component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import faqsData from '../fixtures/faqs';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 
 export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            {faqsData.map(item => 
+            {faqsData.map((item) => ( 
                 <Accordion.Item>
                     <Accordion.Header key={item.id}>{item.header}</Accordion.Header>
                     <Accordion.Body key={item.id}>{item.body}</Accordion.Body>
                 </Accordion.Item>
-            )}
+            ))}
+            <Accordion.Item />
+
+            <OptForm>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+                <OptForm.Break />
+                <OptForm.Input placeholder="Email address" />
+                <OptForm.Button>GET STARTED</OptForm.Button>
+            </OptForm>
+
         </Accordion>
     );
 }
