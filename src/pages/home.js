8e57bc584109ecb399a5b55fcc3629217dfc6e9f
dPacc <themdk01@gmail.com>
2020-10-01 19:24:37 +0400

    added header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { HeaderContainer } from '../containers/header';
+import { Feature, OptForm } from '../components';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
@@ -6,6 +8,18 @@ import { FooterContainer } from '../containers/footer';
 export default function Home() {
     return (
         <>
+            <HeaderContainer >
+                <Feature>
+                    <Feature.Title>Unlimited movies, TV shows, and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel anytime.</Feature.SubTitle>
+                    <OptForm>
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+                        <OptForm.Break />
+                        <OptForm.Input placeholder="Email address" />
+                        <OptForm.Button>GET STARTED</OptForm.Button>
+                    </OptForm>
+                </Feature>
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
