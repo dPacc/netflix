10d572eb34eb0676f2b83b433931476b020de68e
dPacc <themdk01@gmail.com>
2020-10-04 23:14:09 +0400

    added useContent custom hook

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Signup() {
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+    console.log(series);
+    console.log(films);
+
     return (
         <div>
             <p>Hello from Browse</p>
