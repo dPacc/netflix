78e39f7ce775c0b33d876c4438b6e22c0898899b
dPacc <themdk01@gmail.com>
2020-10-11 12:57:45 +0400

    added fuse search:

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
 import { Loading, Header, Card, Player } from '../components';
@@ -26,6 +27,17 @@ export function BrowseContainer({ slides }) {
         setSlideRows(slides[category]);
     }, [slides, category]);
 
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, {keys: ['data.description', 'data.title', 'data.genre']
+        });
+        const results = fuse.search(searchTerm).map(({ item }) => item);
+
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+    }, [searchTerm])
 
     return profile.displayName ? (
         <>
